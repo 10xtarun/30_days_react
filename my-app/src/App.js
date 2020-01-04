103392a651dba0f36178d785922ff3955a89c1d8
CodeSlayerTarun <singhtarun020@gmail.com>
2020-01-04 22:54:10 +0530

    first react render

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+}
+
+export default App;
